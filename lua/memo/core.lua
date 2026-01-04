094bb25200f26a96f88e4a591d280c50f9a10dec
Lenny <lenny.donnez@gmail.com>
2026-01-04 04:24:33 +0100

    fix: ensure no new lines are appended when decrypting

--- lua/memo/core.lua
+++ lua/memo/core.lua
@@ -25,51 +25,58 @@ function M.decrypt_to_stdout(path)
 	return gpg.exec_with_gpg_auth({ "memo", "decrypt", path })
 end
 
+--- Appends lines to buffer
+--- @param bufnr integer
+--- @param lines string[]
+--- @param state { first_write: boolean }
+local function append_to_buffer(bufnr, lines, state)
+	if #lines == 0 or not vim.api.nvim_buf_is_valid(bufnr) then
+		return
+	end
+
+	vim.bo[bufnr].modifiable = true
+	if state.first_write then
+		vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+		state.first_write = false
+	else
+		vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, lines)
+	end
+	vim.bo[bufnr].modifiable = false
+	vim.bo[bufnr].modified = false
+end
+
 --- Decrypts a file and handles all buffer insertions.
 --- @param path string The path to the encrypted file.
 --- @param bufnr integer The buffer handle to write into.
 --- @param on_exit fun(result: vim.SystemCompleted)
 --- @return vim.SystemObj?
 function M.decrypt_to_buffer(path, bufnr, on_exit)
+	local accumulator = ""
+	local state = { first_write = true }
+
 	return gpg.exec_with_gpg_auth({ "memo", "decrypt", path }, {
 		stdout = function(_, data)
 			if not data or data == "" then
 				return
 			end
 
+			accumulator = accumulator .. data
+
 			vim.schedule(function()
-				if not vim.api.nvim_buf_is_valid(bufnr) then
-					return
-				end
-
-				vim.bo[bufnr].modifiable = true
-
-				local line_count = vim.api.nvim_buf_line_count(bufnr)
-				local last_line_idx = line_count - 1
-				local last_line_content = vim.api.nvim_buf_get_lines(bufnr, -2, -1, false)[1] or ""
-				local last_col = #last_line_content
-
-				vim.api.nvim_buf_set_text(
-					bufnr,
-					last_line_idx,
-					last_col,
-					last_line_idx,
-					last_col,
-					vim.split(data, "\n", { plain = true })
-				)
-
-				-- Ensure cursor stays on top
-				local winid = vim.fn.bufwinid(bufnr)
-				if winid ~= -1 then
-					vim.api.nvim_win_set_cursor(winid, { 1, 0 })
-				end
-
-				vim.bo[bufnr].modified = false
-				vim.bo[bufnr].modifiable = false
+				local lines = vim.split(accumulator, "\n", {
+					plain = true,
+				})
+				accumulator = table.remove(lines)
+				append_to_buffer(bufnr, lines, state)
 			end)
 		end,
 	}, function(result)
 		vim.schedule(function()
+			if result.code == 0 and accumulator ~= "" then
+				append_to_buffer(bufnr, { accumulator }, state)
+			end
+			vim.bo[bufnr].modifiable = false
+			vim.bo[bufnr].modified = false
 			on_exit(result)
 		end)
 	end)
